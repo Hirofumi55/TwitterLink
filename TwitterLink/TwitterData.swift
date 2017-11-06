7ae4c1a71efe4c8e5cab29dcad07d2376c8b660a
Hirofumi <sasa1235711@gmail.com>
2017-11-06 22:59:51 +0900

    tableview部分の大体の実装完了

---
+++ TwitterLink/TwitterData.swift
@@ -0,0 +1,31 @@
+//
+//  TwitterData.swift
+//  TwitterLink
+//
+//  Created by Sasakura Hirofumi on 2017/11/06.
+//  Copyright © 2017年 Sasakura Hirofumi. All rights reserved.
+//
+
+import Foundation
+
+class TwitterData {
+    
+    // HashMapの定義
+    // ここは都度更新の必要があり、今後どのようにしていくかは考えなくてはいけない
+    static let twitterMap = ["堀江貴文":"takapon_jp",
+                      "Now or Never":"catnap707",
+                      "落合陽一":"ochyai",
+                      "徳丸浩":"ockeghem",
+                      " Vitalik Buterin":"vitalikbuterin",
+                      "Jeff Bezos":"jeffbezos",
+                      "Jihan Wu":"jihanwu",
+                      "Elon Musk":"elonmusk"
+    ]
+    
+    // TwitterのFQDN
+    static let urlString = "https://twitter.com"
+    
+    // アクセスするTwitterのURL
+    static var url = "https://twitter.com"
+    
+}
--- TwitterLink/ViewController.swift
+++ TwitterLink/ViewController.swift
@@ -8,17 +8,67 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var twitterTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // tableViewを使用するための準備
+        // ここはお決まりの書き方
+        self.twitterTableView.delegate = self
+        self.twitterTableView.dataSource = self
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // セクション数
+    func numberOfSections(in tableView: UITableView) -> Int {
+        // 現状セクションは一つ
+        return 1
+    }
+    
+    // テーブルセル数
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // HashMapの個数分返す
+        let number = TwitterData.twitterMap.count
+        return number
+    }
+    
+    // テーブルセルの中身について
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = self.twitterTableView.dequeueReusableCell(withIdentifier: "twitterCell", for: indexPath)
+        
+        let name = Array(TwitterData.twitterMap.keys)
+        let path = Array(TwitterData.twitterMap.values)
+        
+        
+        cell.textLabel?.text = name[indexPath.row]
+        cell.detailTextLabel?.text = path[indexPath.row]
+        
+        return cell
+    }
+    
+    // セルがタップ
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+        
+        // アクセス先となるURLを生成する
+        let path = cell?.detailTextLabel?.text
+        TwitterData.url = TwitterData.urlString + path
+        
+        
+        
+    }
+    
+    
 
 
 }
